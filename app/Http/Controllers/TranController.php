2f9a1e009eac70eda3d120775727e9b0383348d4
Jhean Carlos Guerrero <jheancg@gmail.com>
2018-08-28 17:52:54 -0300

    fix

--- app/Http/Controllers/TranController.php
+++ app/Http/Controllers/TranController.php
@@ -43,13 +43,8 @@ class TranController extends Controller
             ->where('canal','=',$canal)
             ->get();
 
-        $x = 0;
-        foreach ($trans as $t) {
-          $x += $t->calc;
-        }
 
-
-        return view('tran.abc',compact('trans','x','suma'));
+        return view('tran.abc',compact('trans','suma'));
     }
 
     /**
