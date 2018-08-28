0e1d2055ac47c18cac919c04fc44678ac155b6db
Jhean Carlos Guerrero <jheancg@gmail.com>
2018-08-28 16:41:27 -0300

    crear proveedor

--- resources/views/proveedor/create.blade.php
+++ resources/views/proveedor/create.blade.php
@@ -4,29 +4,29 @@
 
 <section class="content">
     <h1>
-        Create proveedor
+        Agregar Proveedor
     </h1>
-    <a href="{!!url('proveedor')!!}" class = 'btn btn-danger'><i class="fa fa-home"></i> Proveedor Index</a>
+    <a href="{!!url('proveedor')!!}" class = 'btn btn-danger'><i class="fa fa-home"></i> Lista de Proveedores</a>
     <br>
     <form method = 'POST' action = '{!!url("proveedor")!!}'>
         <input type = 'hidden' name = '_token' value = '{{Session::token()}}'>
         <div class="form-group">
-            <label for="cod_prov">cod_prov</label>
-            <input id="cod_prov" name = "cod_prov" type="text" class="form-control">
+            <label for="cod_prov">Código de Proveedor</label>
+            <input id="cod_prov" name = "cod_prov" type="text" class="form-control" required placeholder="Código de Proveedor">
         </div>
         <div class="form-group">
-            <label for="nombre_prov">nombre_prov</label>
-            <input id="nombre_prov" name = "nombre_prov" type="text" class="form-control">
+            <label for="nombre_prov">Nombre del Proveedor</label>
+            <input id="nombre_prov" name = "nombre_prov" type="text" class="form-control" required placeholder="Nombre del Proveedor">
         </div>
         <div class="form-group">
-            <label for="leedt_prov">leedt_prov</label>
-            <input id="leedt_prov" name = "leedt_prov" type="text" class="form-control">
+            <label for="leedt_prov">Leed Time Proveedor</label>
+            <input id="leedt_prov" name = "leedt_prov" type="text" class="form-control" required placeholder="Leed Time Proveedor">
         </div>
         <div class="form-group">
-            <label for="tentrega_prov">tentrega_prov</label>
-            <input id="tentrega_prov" name = "tentrega_prov" type="text" class="form-control">
+            <label for="tentrega_prov">Tiempo de entrega de Proveedor</label>
+            <input id="tentrega_prov" name = "tentrega_prov" type="text" class="form-control" required placeholder="Tiempo de entrega de Proveedor">
         </div>
-        <button class = 'btn btn-success' type ='submit'> <i class="fa fa-floppy-o"></i> Save</button>
+        <button class = 'btn btn-success' type ='submit'> <i class="fa fa-floppy-o"></i> Guardar</button>
     </form>
 </section>
 @endsection
