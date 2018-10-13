d6bb46093b9443d4c7219a15956b485aeb126f18
Jhean Carlos Guerrero <jheancg@gmail.com>
2018-10-13 07:41:38 -0300

    Seeders para roles y usuarios

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,7 +16,9 @@ class RolesTableSeeder extends Seeder
             array('name' => 'Administrador', 'guard_name' => 'web'),
             array('name' => 'Analista', 'guard_name' => 'web'),
         );
-
+        DB::table('roles')->delete();
+        //resetea el autoincremento a 1
+        DB::statement("ALTER SEQUENCE roles_id_seq RESTART WITH 1");
         DB::table('roles')->insert($roles);
 
     }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,8 +14,11 @@ class UsersTableSeeder extends Seeder
         $users = array(
             array('name' => 'seven', 'email' => 'jheancg@gmail.com', 'password' => bcrypt('123456')),
             array('name' => 'Alain Diaz', 'email' => 'jadcve@gmail.com', 'password' => bcrypt('123456')),
-            array('name' => 'Erick Velasquez', 'email' => 'erik@gmail.com', 'password' => bcrypt('123456')),
+            array('name' => 'Erick Velasquez', 'email' => 'erikvelasquez.25@gmail.com', 'password' => bcrypt('123456')),
         );
-        DB::table('users')->insert();
+        DB::table('users')->delete();
+        //resetea el autoincremento a 1
+        DB::statement("ALTER SEQUENCE users_id_seq RESTART WITH 1");
+        DB::table('users')->insert($users);
     }
 }
